f2b5adf22a2b72e357ca7b23d4fbf2ef377b82b8
青城 <118874466+WWJqingcheng@users.noreply.github.com>
2022-12-02 14:01:16 +0800

    Update telecom_live_lotter.py

--- telecom_live_lotter.py
+++ telecom_live_lotter.py
@@ -207,7 +207,7 @@ def get_data(url):
 def main(phone, password):
     apiType = 1
     try:
-        url = "https://gitcode.net/weixin_52142858/telecomliveinfo/-/raw/master/telecomLiveInfo.json"
+        url = "https://gitee.com/kele2233/genxin/raw/master/telecomLiveInfo.json"
         data = get(url, timeout=5).json()        
     except:
         data = list_d
