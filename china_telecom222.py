97b0f8ec5ba03d266f781424520758509f74cdeb
青城 <118874466+WWJqingcheng@users.noreply.github.com>
2022-12-02 21:37:20 +0800

    Update china_telecom222.py

--- china_telecom222.py
+++ china_telecom222.py
@@ -4,7 +4,7 @@
 # @Author : github@limoruirui https://github.com/limoruirui
 # @Time : 2022/9/12 16:10
 # cron "1 9,12 * * *" script-path=xxx.py,tag=匹配cron用
-# const $ = new Env('电信签到多账号');
+# const $ = new Env('电信签到');
 # -------------------------------
 
 """
